440d00c82eec95d5fe410954cff2a3df21e29eaa
Matt Warman <mkwarman@gmail.com>
2023-08-13 22:04:43 -0400

    Update header

--- client/src/pages/resume/index.tsx
+++ client/src/pages/resume/index.tsx
@@ -3,7 +3,7 @@ import { Paper } from "@mui/material"
 export const Resume = () => {
   return (
     <Paper sx={{height: "85vh"}}>
-      <object data="/static/MattWarmanResumePublic.pdf" type="application/pdf" width="100%" height="100%"></object>
+      <object data="/static/MattWarmanResume.pdf" type="application/pdf" width="100%" height="100%"></object>
     </Paper>
   )
 }
